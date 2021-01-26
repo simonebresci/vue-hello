8ddd6ec89c7cdde6c29d05aa7ea7109b1ffa773a
Simone <simone.bresci.05@gmail.com>
2021-01-26 16:27:46 +0100

    inizio bonus

---
+++ js/main-bonus.js
@@ -0,0 +1,26 @@
+
+
+var app = new Vue({
+  el: '#root',
+  data: {
+
+    // Colore 1
+    box1Name : 'BOX 1',
+    colore1R : 0,
+    colore1G : 0,
+    colore1B : 0,
+    colore1T : 1,
+    // Colore 2
+    box2Name : 'BOX 2',
+    colore2R : 0,
+    colore2G : 0,
+    colore2B : 0,
+    colore2T : 1
+  }
+
+
+
+});
+// Abilita i dev tools
+Vue.config.devtools = true;
+// BONUS
--- js/main.js
+++ js/main.js
@@ -14,19 +14,3 @@ var app = new Vue({
 });
 // Abilita i dev tools
 Vue.config.devtools = true;
-
-
-// BONUS
-// data: {
-//   // Colore 1
-//   colore1R : 0,
-//   colore1G : 0,
-//   colore1B : 0,
-//   colore1T : 0,
-//   // Colore 2
-//   colore2R : 0,
-//   colore2G : 0,
-//   colore2B : 0,
-//   colore2T : 0
-// }
-// DATA
