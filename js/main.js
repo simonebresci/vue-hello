c8fd6cab8c0d83593296b89f260600f8c5b45f28
Simone <simone.bresci.05@gmail.com>
2021-01-26 15:51:19 +0100

    Vue - h1 + pathImmagine

---
+++ js/main.js
@@ -0,0 +1,32 @@
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+// Aggiungere un’immagine presa anch’essa da un data
+
+var app = new Vue({
+  el: '#root',
+  data: {
+    messaggio: 'Vue HELLO!',
+    pathImg: '',
+    animale: 'pinguino'
+  }
+
+
+
+});
+// Abilita i dev tools
+Vue.config.devtools = true;
+
+
+// BONUS
+// data: {
+//   // Colore 1
+//   colore1R : 0,
+//   colore1G : 0,
+//   colore1B : 0,
+//   colore1T : 0,
+//   // Colore 2
+//   colore2R : 0,
+//   colore2G : 0,
+//   colore2B : 0,
+//   colore2T : 0
+// }
+// DATA
