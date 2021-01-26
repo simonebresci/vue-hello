fb9f7d74964d60cbf2a77580dae9d686a715c12b
Simone <simone.bresci.05@gmail.com>
2021-01-26 18:43:00 +0100

    rgb selector + immagini + bordi

--- js/main-bonus.js
+++ js/main-bonus.js
@@ -1,4 +1,10 @@
 
+// Idee
+// - aggiungere immagini
+// - evento click + methods
+//
+
+
 
 var app = new Vue({
   el: '#root',
@@ -6,6 +12,8 @@ var app = new Vue({
 
     // Colore 1
     box1Name : 'BOX 1',
+    box1Img: '',
+    box1BorderRadius: 10,
     colore1R : 0,
     colore1G : 0,
     colore1B : 0,
@@ -16,6 +24,48 @@ var app = new Vue({
     colore2G : 0,
     colore2B : 0,
     colore2T : 1
+  },
+  methods:{
+    subtract10red: function(){
+      this.colore1R -= 10;
+    },
+    add10red: function(){
+      this.colore1R += 10;
+    },
+    subtract10green: function(){
+      this.colore1G -= 10;
+    },
+    add10green: function(){
+      this.colore1G += 10;
+    },
+    subtract10blue: function(){
+      this.colore1B -= 10;
+    },
+    add10blue: function(){
+      this.colore1B += 10;
+    },
+    subtract10BorderRadius: function(){
+      this.box1BorderRadius -= 10;
+    },
+    add10BorderRadius: function(){
+      this.box1BorderRadius += 10;
+    },
+    subtract1transp: function(){
+      this.colore1T -= 0.01;
+    },
+    add1transp: function(){
+      this.colore1T += 0.01;
+    },
+    selectPinguino: function(){
+      this.box1Img = 'pinguino';
+    },
+    selectLeone: function(){
+      this.box1Img = 'leone';
+    },
+    selectAlpaca: function(){
+      this.box1Img = 'alpaca';
+    },
+
   }
 
 
